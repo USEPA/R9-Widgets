56a7fc8b62875d77a588237575f4ceeafd1d1bf2
Travis Bock <tbock@innovateteam.com>
2020-12-10 16:52:11 -0800

    removing sdwis from master b/c this isn't the real one

--- widgets/SDWISwidget/nls/strings.js
+++
@@ -1,9 +0,0 @@
-define({
-  root: {
-    _widgetLabel: 'SDWIS Widget',
-    widgetTitle: 'SDWIS Widget',
-    description: 'A custom Web AppBuilder widget.'
-  }
-  // add supported locales below:
-  // , "zh-cn": true
-});
