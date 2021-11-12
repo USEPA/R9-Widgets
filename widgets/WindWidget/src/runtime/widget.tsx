1cdf160e8a68a2bc70deb07e3aed0e841ff3c12b
Keaton Shennan <kshennan@innovateteam.com>
2021-11-12 16:45:55 -0600

    in progress wind widget updates, added dropdown/layer handling

--- widgets/WindWidget/src/runtime/widget.tsx
+++ widgets/WindWidget/src/runtime/widget.tsx
@@ -1,17 +1,16 @@
 /** @jsx jsx */
-import {AllWidgetProps, BaseWidget, jsx} from "jimu-core";
+import {AllWidgetProps, BaseWidget, getAppStore, jsx, WidgetState} from "jimu-core";
 import {IMConfig} from "../config";
+import './assets/current_wind_gfs.json';
 
-
-// import { TabContent, TabPane, Nav, NavItem, NavLink, Button} from 'jimu-ui';
-import defaultMessages from "./translations/default";
-
-import {Progress} from 'jimu-ui';
+import {Dropdown, DropdownButton, DropdownItem, DropdownMenu} from 'jimu-ui';
 import {Component} from 'react';
 import {JimuMapView, JimuMapViewComponent, MapViewManager} from 'jimu-arcgis';
 import FeatureLayer from 'esri/layers/FeatureLayer';
 import execute from "esri/rest/query";
 import Extent from 'esri/geometry/Extent';
+import moment from 'Moment';
+
 import {
     AnimatedEnvironmentLayer,
     DisplayOptions,
@@ -21,17 +20,27 @@ import {
 } from "./animatedEnvironmentLayer";
 
 export default class WindWidget extends BaseWidget<AllWidgetProps<IMConfig>, {}> {
-    url = 'https://localhost:3001/widgets/WindWidget/dist/global-wind.json';
+    // url = 'https://localhost:3001/widgets/WindWidget/dist/global-wind.json';
+    // wind data urls
+    // hrrr: https://r9data.response.epa.gov/apps/wind_data/current_wind_hrrr.json
+    // nam: https://r9data.response.epa.gov/apps/wind_data/current_wind_nam.json
+    // gfs: https://r9data.response.epa.gov/apps/wind_data/current_wind_gfs.json
+    hrrrUrl = `${this.props.context.folderUrl}dist/runtime/assets/current_wind_hrrr.json`
+    namUrl = 'https://localhost:3001/widgets/WindWidget/dist/assets/current_wind_nam.json';
+    gfsUrl = 'https://localhost:3001/widgets/WindWidget/dist/assets/current_wind_gfs.json';
     displayOptions: DisplayOptions = {
         maxVelocity: 15
     }
+
+    selectedModel: string = "HRRR";
+
     environmentLayer = new AnimatedEnvironmentLayer({
         id: "ael-layer",
-        url: this.url,
+        url: this.hrrrUrl,
         displayOptions: this.displayOptions
     });
     jmv: JimuMapView;
-
+    _forecast_datetime
 
 
     onActiveViewChange = (jmv: JimuMapView) => {
@@ -48,9 +57,86 @@ export default class WindWidget extends BaseWidget<AllWidgetProps<IMConfig>, {}>
         this.jmv.view.map.remove(this.environmentLayer);
     }
 
+    componentDidUpdate(prevProps: Readonly<AllWidgetProps<IMConfig>>, prevState: Readonly<{}>, snapshot?: any) {
+
+        let widgetState: WidgetState;
+        widgetState = getAppStore().getState().widgetsRuntimeInfo[this.props.id].state;
+        if (widgetState == WidgetState.Opened) {
+
+
+        } else if (widgetState == WidgetState.Closed) {
+            // // do stuff here on widget open if needed
+
+        }
+
+    }
+
+    getArbitraryFirstMapWidgetId = (): string => {
+        const appState: any = window._appState;
+        // Loop through all the widgets in the config and find the "first"
+        // that has the type (uri) of "arcgis-map"
+        const arbitraryFirstMapWidgetInfo: { [key: string]: any } = Object.values(appState.appConfig.widgets).find((widgetInfo: any) => {
+            return widgetInfo.uri === 'widgets/arcgis/arcgis-map/'
+        });
+
+        return arbitraryFirstMapWidgetInfo.id;
+    }
+
+    _setWindModel(model) {
+        this.selectedModel = model;
+
+        this.setState({
+            selectedModel: this.selectedModel
+        });
+
+        let layerUrl = 'HRRR' ? this.hrrrUrl : 'NAM' ? this.namUrl : this.gfsUrl;
+        layerUrl
+
+        this.jmv.view.map.layers.remove(this.environmentLayer);
+
+        this.environmentLayer = new AnimatedEnvironmentLayer({
+            id: "ael-layer",
+            url: layerUrl,
+            displayOptions: this.displayOptions
+        });
+        this.jmv.view.map.layers.add(this.environmentLayer);
+        this._updateForecast(this.environmentLayer);
+        return model;
+    }
+
+    _updateForecast(forecastData) {
+        
+        this._forecast_datetime = moment(forecastData.date).add(forecastData.date, 'hours').format('ll hA');
+        this.setState({
+            _forecast_datetime: this._forecast_datetime
+        });
+
+    }
 
     render() {
-        return <JimuMapViewComponent useMapWidgetId={this.props.useMapWidgetIds?.[0]}
-                                     onActiveViewChange={this.onActiveViewChange}/>
+        return (
+            <div className="widget-addLayers jimu-widget p-2" style={{overflow: "auto"}}>
+                <div id='forecast-data'> {this._forecast_datetime}</div>
+                Wind Model:
+                <Dropdown direction='down'>
+                    <DropdownButton>
+                        {this.selectedModel}
+                    </DropdownButton>
+                    <DropdownMenu>
+                        <DropdownItem active onClick={() => {
+                            this._setWindModel('HRRR')
+                        }}>HRRR</DropdownItem>
+                        <DropdownItem onClick={() => {
+                            this._setWindModel('NAM')
+                        }}>NAM</DropdownItem>
+                        <DropdownItem onClick={() => {
+                            this._setWindModel('GFS')
+                        }}>GFS</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+                <JimuMapViewComponent useMapWidgetId={this.getArbitraryFirstMapWidgetId()}
+                                      onActiveViewChange={this.onActiveViewChange}/>
+            </div>
+        );
     }
 }
