29b66467b67d33bfc9fa1d12eb48af2a82714266
Travis Bock <tbock@innovateteam.com>
2022-09-07 10:24:23 -0700

    incomplete changes to wind/smoke

--- widgets/TimeWidget/src/runtime/widget.tsx
+++ widgets/TimeWidget/src/runtime/widget.tsx
@@ -17,12 +17,14 @@
  A copy of the license is available in the repository's
  LICENSE file.
  */
-import {React, AllWidgetProps, FormattedMessage} from 'jimu-core'
+import {React, AllWidgetProps, FormattedMessage, DataSourceComponent} from 'jimu-core'
 import {JimuMapViewComponent, JimuMapView} from 'jimu-arcgis'
 
 import moment from 'Moment'
 import TimeSlider from 'esri/widgets/TimeSlider'
 import TimeExtent from 'esri/TimeExtent';
+import {Select, Option, Switch} from 'jimu-ui';
+import {getAppStore, WidgetState} from 'jimu-core';
 
 const {useState, useRef, useEffect} = React
 
@@ -33,14 +35,10 @@ function initTimeSlider(view, fullTimeExtent) {
   container.setAttribute('style', 'bottom: 30px; width: 80%; margin: 0 10% 0 10%;')
   const newTimeSlider = new TimeSlider({
     container,
-    view,
+    // view,
     mode: 'time-window',
     fullTimeExtent,
     timeVisible: true,
-    timeExtent: {
-      start,
-      end
-    },
     stops: {
       interval: {
         value: 1,
@@ -48,11 +46,19 @@ function initTimeSlider(view, fullTimeExtent) {
       }
     }
   })
+  // newTimeSlider.postInitialize = () => {
+  //   setTimeout(() => {
+  //     newTimeSlider.timeExtent = {
+  //       start,
+  //       end
+  //     }
+  //   })
+  // }
   view.ui.add(newTimeSlider, 'manual');
   return newTimeSlider;
 }
 
-export default function ({useMapWidgetIds}: AllWidgetProps<{}>) {
+export default function ({useMapWidgetIds, windDataSource, smokeDataSource, id}: AllWidgetProps<{}>) {
   const [jimuMapView, setJimuMapView] = useState<JimuMapView>(null)
   const [timeLayers, setTimeLayers] = useState([])
   const [timeSlider, setTimeSlider] = useState()
@@ -60,7 +66,11 @@ export default function ({useMapWidgetIds}: AllWidgetProps<{}>) {
     start: null,
     end: null
   })
-
+  const [windLayers, setWindLayers] = useState([])
+  const [smokeLayer, setSmokeLayer] = useState([])
+  const [smokeVisible, setSmokeVisible] = useState(false)
+  const [windGroupLayer, setWindGroupLayer] = useState(null)
+  const [smokeGroupLayer, setSmokeGroupLayer] = useState(null)
 
   useEffect(() => {
     if (jimuMapView && timeExtent.start && timeExtent.end) {
@@ -74,6 +84,13 @@ export default function ({useMapWidgetIds}: AllWidgetProps<{}>) {
 
     }
   }, [jimuMapView, timeExtent])
+
+  useEffect(() => {
+    const widgetState: WidgetState = getAppStore().getState().widgetsRuntimeInfo[id].state;
+    if (timeSlider) {
+      timeSlider.visible = widgetState === 'OPENED'
+    }
+  })
 
   useEffect(() => {
     if (timeLayers.length > 0) {
@@ -104,8 +121,29 @@ export default function ({useMapWidgetIds}: AllWidgetProps<{}>) {
       }))
       setTimeLayers(t.filter(l => l.layer.visible))
       // captureCurrentTimeExtent();
+      TimeSlider.getPropertiesFromWebMap(jimuMapView.view.map).then(tss => console.log(tss))
+
     }
   }, [jimuMapView])
+
+  useEffect(() => {
+    if (jimuMapView && windGroupLayer) {
+      setWindLayers(jimuMapView.view.map.allLayers
+        .find(l => l.id === windGroupLayer.id)?.allLayers
+        .filter(l => l.portalItem && l.portalItem.tags.includes('wind')))
+    }
+  }, [jimuMapView, windGroupLayer])
+
+  useEffect(() => {
+    if (jimuMapView && smokeGroupLayer) {
+      const l = jimuMapView.view.map.allLayers
+        .find(l => l.id === smokeGroupLayer.id).allLayers
+        .find(l => l.portalItem && l.portalItem.tags.includes('R9 Smoke'))
+
+      setSmokeLayer(l)
+      setSmokeVisible(l.visible)
+    }
+  }, [jimuMapView, smokeGroupLayer])
 
   const onActiveViewChange = (jmv: JimuMapView) => {
     if (jmv) {
@@ -113,12 +151,48 @@ export default function ({useMapWidgetIds}: AllWidgetProps<{}>) {
     }
   }
 
+  const captureWindLayers = (v) => {
+    setWindGroupLayer(v.layer)
+  }
+  const captureSmokeLayer = (v) => {
+    setSmokeGroupLayer(v.layer)
+  }
+
+  const updateSelectedWindModel = e => {
+    windLayers.forEach(l => l.visible = false)
+    e.target.value.visible = true
+  }
+
   const isConfigured = useMapWidgetIds && useMapWidgetIds.length === 1
 
+  const toggleSmoke = () => {
+    smokeLayer.visible = !smokeLayer.visible
+    setSmokeVisible(smokeLayer.visible)
+  }
   return <div className="widget-use-map-view" style={{width: '100%', height: '100%', overflow: 'hidden'}}>
     <JimuMapViewComponent
       useMapWidgetId={useMapWidgetIds?.[0]}
       onActiveViewChange={onActiveViewChange}
     />
+    <DataSourceComponent
+      useDataSource={windDataSource?.[0]}
+      onDataSourceCreated={captureWindLayers}
+    />
+    <DataSourceComponent
+      useDataSource={smokeDataSource?.[0]}
+      onDataSourceCreated={captureSmokeLayer}
+    />
+    {windLayers
+      ? <Select onChange={updateSelectedWindModel} placeholder="Select Wind Forecast Model">
+        {windLayers.map(l => <Option value={l}>{l.title}</Option>)}
+      </Select>
+      : null}
+    {smokeLayer
+      ? <div style={{"margin": "10px"}}>
+        <Switch checked={smokeVisible} style={{"margin-right": "10px"}}
+                onChange={toggleSmoke}/>
+        Toggle Smoke
+        .</div>
+      : null}
   </div>
 }
--- widgets/TimeWidget/src/setting/setting.tsx
+++ widgets/TimeWidget/src/setting/setting.tsx
@@ -1,30 +1,41 @@
 /**
-  Licensing
+ Licensing
 
-  Copyright 2022 Esri
+ Copyright 2022 Esri
 
-  Licensed under the Apache License, Version 2.0 (the "License"); You
-  may not use this file except in compliance with the License. You may
-  obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
+ Licensed under the Apache License, Version 2.0 (the "License"); You
+ may not use this file except in compliance with the License. You may
+ obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-  implied. See the License for the specific language governing
-  permissions and limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ implied. See the License for the specific language governing
+ permissions and limitations under the License.
 
-  A copy of the license is available in the repository's
-  LICENSE file.
-*/
-import { React } from 'jimu-core';
-import { MapWidgetSelector } from 'jimu-ui/advanced/setting-components';
-import { AllWidgetSettingProps } from 'jimu-for-builder';
+ A copy of the license is available in the repository's
+ LICENSE file.
+ */
+import {React, UseDataSource, Immutable} from 'jimu-core';
+import {MapWidgetSelector} from 'jimu-ui/advanced/setting-components';
+import {AllWidgetSettingProps} from 'jimu-for-builder';
+import {AllDataSourceTypes, DataSourceSelector} from 'jimu-ui/advanced/data-source-selector';
+import {TextArea} from 'jimu-ui';
 
 export default function Setting(
   props: AllWidgetSettingProps<{}>
 ): React.ReactElement {
 
+  const supportedTypes = Immutable([AllDataSourceTypes.GroupLayer]);
+
+  const updateConfigProperty = (property, value) => {
+    let settings = {
+      id: props.id
+    }
+    settings[property] = value;
+    props.onSettingChange(settings);
+  }
   const onMapSelected = (useMapWidgetIds: string[]) => {
     props.onSettingChange({
       id: props.id,
@@ -32,7 +43,41 @@ export default function Setting(
     });
   }
 
+  const onDataSourceChange = (useDataSources: UseDataSource[]) => {
+    props.onSettingChange({
+      id: props.id,
+      windDataSources: useDataSources,
+    });
+  }
+
+  // cannot use data source select b/c it doesn't support image services?!?
   return <div className="sample-js-api-widget-setting p-2">
-    <MapWidgetSelector onSelect={onMapSelected} useMapWidgetIds={props.useMapWidgetIds} />
+    <MapWidgetSelector onSelect={onMapSelected} useMapWidgetIds={props.useMapWidgetIds}/>
+    <DataSourceSelector
+      types={supportedTypes}
+      mustUseDataSource={true}
+      useDataSources={props.windDataSource}
+      onChange={v => updateConfigProperty('windDataSource', v)}
+      widgetId={props.id}
+      buttonLabel="Select Wind Group"
+    />
+    <DataSourceSelector
+      types={supportedTypes}
+      mustUseDataSource={true}
+      useDataSources={props.smokeDataSource}
+      onChange={v => updateConfigProperty('smokeDataSource', v)}
+      widgetId={props.id}
+      buttonLabel="Select Smoke Group"
+    />
+    {/*<DataSourceSelector*/}
+    {/*  types={supportedTypes}*/}
+    {/*  mustUseDataSource={true}*/}
+    {/*  useDataSources={props.useDataSources}*/}
+    {/*  onChange={v => updateConfigProperty('updateConfigProperty', v)}*/}
+    {/*  widgetId={props.id}*/}
+    {/*  buttonLabel="Select Wind Group"*/}
+    {/*/>*/}
+    {/*<TextArea placeholder='GFS Wind Service' value={props.permissionsListId}*/}
+    {/*          onChange={e => updateConfigProperty('gfs', e.target.value)}/>*/}
   </div>
 }
